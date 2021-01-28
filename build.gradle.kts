e9c778c70ddd50e47d4de334419136ea7d6fdb6f
Luiz Reis <luiz.reis@ip-10-250-250-34.ec2.internal>
2021-01-28 15:59:40 -0300

    add order module

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
