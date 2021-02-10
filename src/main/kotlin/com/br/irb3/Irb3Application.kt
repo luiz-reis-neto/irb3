b02af3f0a2197633428a1106076fbea5315613a3
Luiz Reis <luiz.reis@LOFT-SPM-0157.local>
2021-02-10 14:22:40 -0300

    feat: add cors origin

--- src/main/kotlin/com/br/irb3/Irb3Application.kt
+++ src/main/kotlin/com/br/irb3/Irb3Application.kt
@@ -2,10 +2,26 @@ package com.br.irb3
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
 
 @SpringBootApplication
 class Irb3Application
 
 fun main(args: Array<String>) {
 	runApplication<Irb3Application>(*args)
+}
+
+@Bean
+fun corsConfigurer(): WebMvcConfigurer? {
+	return object : WebMvcConfigurer {
+		override fun addCorsMappings(registry: CorsRegistry) {
+			registry.addMapping("/**").allowedOrigins("*")
+		}
+	}
 }
